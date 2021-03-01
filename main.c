f3dce0924c838b4e693d81d02150e26bd7a7148f
Calebe Micael <calebemicael@gmail.com>
2021-03-01 22:41:48 +0000

    Resposta para a questao 00

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void) {
-  printf("Ola Mundo\n");
-  return 0;
-}
+// int main(void) {
+//   printf("Ola Mundo\n");
+//   return 0;
+// }
---
+++ questao00/questao00.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+  printf("Sem spoiler\n");
+}
